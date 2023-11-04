735f097f5f3aedacb1187252b655e4e0dd2e0b4a
Jonathan Kong <275568635@qq.com>
2023-11-04 20:27:40 +0800

    add matrix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,12 +15,12 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
+          {/* <Link
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
             Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          </Link> */}
         </div>
       </div>
     </header>
@@ -36,7 +36,9 @@ export default function Home(): JSX.Element {
     >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <div style={{ fontSize: "20px", textAlign: "center", padding: "40px" }}>
+          I'm a java web full stack engineer.
+        </div>
       </main>
     </Layout>
   )
