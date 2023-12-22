397e44e74e5e8b251aa75391ed836b6bf6186258
Jonathan <275568635@qq.com>
2023-12-22 12:09:58 +0800

    migrate code.

--- src/pages/demo/react/demo23.child1.classcomponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import MyContext from "./demo23.context"
-export default class ChildComponent extends React.Component {
-  static contextType = MyContext
-  render() {
-    let { val1 } = this.context
-    return <div>{val1}</div>
-  }
-}
