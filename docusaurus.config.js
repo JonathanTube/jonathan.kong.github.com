a2632655d0266fc941fe49b701488b6bbe9e9498
Jonathan Kong <275568635@qq.com>
2024-04-16 12:23:55 +0800

    debugging

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://jonathan.kong.github.io",
+  url: "https://jonathantube.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Jonathan.Kong", // Usually your GitHub org/user name.
-  projectName: "Jonathan.Kong.github.com", // Usually your repo name.
+  organizationName: "JonathanTube", // Usually your GitHub org/user name.
+  projectName: "jonathan.kong.github.com", // Usually your repo name.
   deploymentBranch: "deploy",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
