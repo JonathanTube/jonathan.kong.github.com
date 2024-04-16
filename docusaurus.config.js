0a88ae15cfb1d847080e66468c852edda81a7169
Jonathan Kong <275568635@qq.com>
2024-04-16 12:13:17 +0800

    change repo name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://jonathantube.github.io",
+  url: "https://jonathan.kong.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "JonathanTube", // Usually your GitHub org/user name.
-  projectName: "jonathantube.github.com", // Usually your repo name.
+  organizationName: "Jonathan.Kong", // Usually your GitHub org/user name.
+  projectName: "Jonathan.Kong.github.com", // Usually your repo name.
   deploymentBranch: "deploy",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/JonathanTube/jonathantube.github.com/tree/main"
+            "https://github.com/JonathanTube/Jonathan.Kong.github.com/tree/main"
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/JonathanTube/jonathantube.github.com/tree/main"
+            "https://github.com/JonathanTube/Jonathan.Kong.github.com/tree/main"
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css")
